390f9e360917ea66f890949701a37912021aa2c6
gabrielluizs1996 <gabrielluizs1996@gmail.com>
2021-03-29 14:06:15 -0300

    Initial commit

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  constructor(private container: ViewContainerRef, 
+    private template: TemplateRef<any>) {}
+  
+    ngOnInit(): void {
+  }
+}
